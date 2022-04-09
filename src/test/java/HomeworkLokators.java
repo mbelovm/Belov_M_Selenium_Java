71cb4e91aace5ed32d34f11fe8ed1ddfa4e682d3
mbelov <Mikhail.Belov@telekom.com>
2022-04-09 17:07:31 +0300

    First commit

---
+++ src/test/java/HomeworkLokators.java
@@ -0,0 +1,49 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomeworkLokators {
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+        // driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void LokatorsTest() {
+        driver.get("https://www.way2automation.com/way2auto_jquery/registration.php#load_box");
+
+        driver.findElement(By.cssSelector("p>input[name=\"name\"]")).click();
+        driver.findElement(By.xpath("//p[./label[contains(text(), \"Last Name:\")]]/input")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Marital Status:\")]]/div/label[contains(text(), \" Single\")]/input")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Marital Status:\")]]/div/label[contains(text(), \" Married\")]/input")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Marital Status:\")]]/div/label[contains(text(), \" Divorced\")]/input")).click();
+        driver.findElement(By.xpath("//fieldset[@class=\"fieldset padding-bottom\"]/div/label[contains(text(), \" Dance\")]/input")).click();
+        driver.findElement(By.xpath("//fieldset[@class=\"fieldset padding-bottom\"]/div/label[contains(text(), \" Reading\")]/input")).click();
+        driver.findElement(By.xpath("//fieldset[@class=\"fieldset padding-bottom\"]/div/label[contains(text(), \" Cricket\")]/input")).click();
+        driver.findElement(By.xpath("//form[@id=\"register_form\"]/fieldset[./label[contains(text(), \"Country\")]]/select")).click();
+        driver.findElement(By.xpath("//form[@id=\"register_form\"]/fieldset[./label[contains(text(), \"Country\")]]/select/option[2]")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Date of Birth:\")]]/div[1]/select/option[2]")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Date of Birth:\")]]/div[2]/select/option[2]")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Date of Birth:\")]]/div[3]/select/option[contains(text(), \"2014\")]")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Phone Number:\")]]/input")).click();
+        driver.findElement(By.xpath("//form[@id=\"register_form\"]/fieldset[./label[contains(text(), \"Username:\")]]/input")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"E-mail:\")]]/input")).click();
+        //driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"Your Profile Picture\")]]/input")).click();
+        driver.findElement(By.xpath("//fieldset[./label[contains(text(), \"About Yourself\")]]/textarea")).click();
+        driver.findElement(By.xpath("//form[@id=\"register_form\"]/fieldset[./label[text()= \"Password:\"]]/input")).click();
+        driver.findElement(By.xpath("//form[@id=\"register_form\"]/fieldset[./label[text()= \"Confirm Password:\"]]/input")).click();
+        driver.findElement(By.cssSelector("input[value=submit]")).click();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,9 +1,15 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class MyTest {
 
@@ -12,6 +18,7 @@ public class MyTest {
     @Before
     public void setup() {
         driver = new ChromeDriver();
+        //wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         // driver = new FirefoxDriver();
     }
 
@@ -21,8 +28,24 @@ public class MyTest {
 //        driver.get("https://www.google.com/doodles");
         driver.get("https://www.google.com");
 //        driver.navigate().to("https://www.google.com");
-        driver.navigate().to("https://www.google.com/doodles");
+       /* driver.navigate().to("https://www.google.com/doodles");
         driver.navigate().to("https://www.google.com");
+      driver.findElement(By.id("company"));
+        driver.findElement(By.tagName("div.Umvnrc"));
+        driver.findElement(By.className("Umvnrc"));
+        driver.findElement(By.cssSelector(".Umvnrc"));
+
+        driver.findElement(By.name("div.Umvnrc"));
+        driver.findElement(By.linkText("div.Umvnrc"));
+        driver.findElement(By.partialLinkText("div.Umvnrc"));
+        driver.findElement(By.xpath("div.Umvnrc"));
+        /* element.click();
+        element.sendKeys("text");
+        List<WebElement> elementList = driver.findElements(By.cssSelector("div.Umvnrc"));*/
+
+        WebElement inputField = driver.findElement(By.cssSelector("input.gLFyf"));
+        driver.navigate().refresh();
+        inputField.click();
     }
 
     @After
